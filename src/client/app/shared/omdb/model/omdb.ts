0e13a3af8dddf8abf05e21e4258bdb24b88b6a83
Elon Mitchell <elon.mitchell@weathermatic.com>
2017-03-28 09:29:33 -0500

    Adding OMDB Service

---
+++ src/client/app/shared/omdb/model/omdb.ts
@@ -0,0 +1,10 @@
+//Definition of a contact item
+export class Omdb {
+    constructor(
+        public Title: String,
+        public Year: String,
+        public imdbID: String,
+        public Type: String,
+        public Poster: String
+    ){}
+}
